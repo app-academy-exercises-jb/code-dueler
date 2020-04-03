52b1472ba1cb7ebe12ee990fdaffea6546e0f87d
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-04-03 11:53:31 -0500

    Style nav bar

--- client/src/components/nav/NavBar.js
+++ client/src/components/nav/NavBar.js
@@ -1,9 +1,19 @@
 import React from "react";
+import LogOutButton from "../users/LogOutButton";
+import { Link } from "react-router-dom";
 
 const NavBar = (props) => {
   return (
     <div className="nav-bar-wrapper">
-      <div className="logo-nav">CodeDueler</div>
+      <div className="left-nav"></div>
+      <div className="logo-wrapper">
+        <Link to="/" className="logo-nav">
+          CodeDueler
+        </Link>
+      </div>
+      <div className="logout-nav">
+        <LogOutButton />
+      </div>
     </div>
   );
 };
