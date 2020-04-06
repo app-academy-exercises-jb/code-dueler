dfde31195dacaf76588218000fe5c48c1d75109a
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-04-06 11:05:48 -0500

    Fix passport

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,18 +1,20 @@
-const JwtStrategy = require('passport-jwt').Strategy,
-  ExtractJwt = require('passport-jwt').ExtractJwt,
-  mongoose = require('mongoose'),
-  User = mongoose.model('User');
+const JwtStrategy = require("passport-jwt").Strategy,
+  ExtractJwt = require("passport-jwt").ExtractJwt,
+  mongoose = require("mongoose"),
+  User = mongoose.model("User");
 
 const options = {};
 options.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 options.secretOrKey = process.env.SECRET_OR_KEY;
 
-module.exports = passport => {
-  passport.use(new JwtStrategy(options, async (token, done) => {
-    if (token) {
-      const user = await User.findById(token._id);
-      return done(null, user);
-    }
-    return done(null, false);
-  }))
+module.exports = (passport) => {
+  passport.use(
+    new JwtStrategy(options, async (token, done) => {
+      if (token) {
+        const user = await User.findById(token._id);
+        return done(null, user);
+      }
+      return done(null, false);
+    })
+  );
 };
