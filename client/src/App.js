d1a14d789e9846b6193f5431188555183c13227e
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-04-07 14:40:49 -0500

    Remove editor from app.js

--- client/src/App.js
+++ client/src/App.js
@@ -7,13 +7,11 @@ import ErrorPage from "./pages/ErrorPage";
 import ProtectedRoute from "./components/util/ProtectedRoute";
 import AuthRoute from "./components/util/AuthRoute";
 import "./stylesheets/application.scss";
-import CodeEditor from "./components/codeEditor/CodeEditor";
 
 export default () => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" component={CodeEditor} />
         <ProtectedRoute exact path="/" component={GlobalLobby} />
         <AuthRoute exact path="/login" component={Login} />
         <AuthRoute exact path="/signup" component={SignUp} />
