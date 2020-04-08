3df6c6ee0a3f107e42354f7f0aaf99881127f809
Thalia <thaliapena12@gmail.com>
2020-04-08 11:36:06 -0700

    Add stats box

--- client/src/pages/PlayerScreen.js
+++ client/src/pages/PlayerScreen.js
@@ -3,12 +3,14 @@ import NavBar from "../components/nav/NavBar";
 import PlayerChat from "../components/player/PlayerChat";
 import ChallengeQuestion from "../components/player/ChallengeQuestion";
 import CodeEditor from "../components/codeEditor/CodeEditor"
+import Stats from "../components/player/Stats";
 
 export default () => {
   return (
     <div className="main">
       <NavBar />
       <ChallengeQuestion />
+      <Stats />
       <PlayerChat />
       <CodeEditor />
     </div>
