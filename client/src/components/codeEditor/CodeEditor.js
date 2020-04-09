4330b69d85593c7f1fc8191c65fa26ead25b719a
Jorge Barreto <jrbd93@gmail.com>
2020-04-09 11:43:12 -0400

    add fetch code for CodeEditor's submit

--- client/src/components/codeEditor/CodeEditor.js
+++ client/src/components/codeEditor/CodeEditor.js
@@ -24,7 +24,21 @@ const CodeEditor = (props) => {
           margin: "1rem",
         }}
         onClick={e => {
-          
+          fetch("https://us-central1-code-dueler.cloudfunctions.net/parseCode", {
+            method: 'POST',
+            mode: 'cors',
+            // e.target.value must have text which defines a function fizzBuzz
+            body: JSON.stringify({data: { codeToRun: e.target.value }}),
+            headers: {
+              'Content-Type': 'application/json',
+              authorization: localStorage.getItem('token'),
+            }
+          })
+            .then(res => res.json())
+            .then(res => {
+              // res is the server response
+            })
+            .catch(err => console.log(err))
         }}
       >
         Submit Code
