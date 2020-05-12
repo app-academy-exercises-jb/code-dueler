397c70681fec1f2f4044eaaee5659290f0d55295
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-05-11 22:21:09 -0500

    Add images to slide show

--- client/src/components/splash/Splash.js
+++ client/src/components/splash/Splash.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 class SplashComponent extends React.Component {
   constructor(props) {
@@ -10,16 +11,20 @@ class SplashComponent extends React.Component {
     this.photo2 = React.createRef();
     this.photo3 = React.createRef();
     this.photo4 = React.createRef();
+    this.photo5 = React.createRef();
+    this.photo6 = React.createRef();
+    this.photo7 = React.createRef();
+    this.photo8 = React.createRef();
   }
 
   moveRight() {
     console.log(this.state.idx);
-    if (this.state.idx < 4) {
+    if (this.state.idx < 8) {
       this[`photo${this.state.idx + 1}`].current.classList.add("card-in");
       this[`photo${this.state.idx}`].current.classList.remove("card-in");
       this[`photo${this.state.idx}`].current.classList.add("card-out");
     }
-    if (this.state.idx < 4) {
+    if (this.state.idx < 8) {
       this.setState({ idx: this.state.idx + 1 });
     }
   }
@@ -41,7 +46,7 @@ class SplashComponent extends React.Component {
     let rightArrow = null;
     if (this.state.idx === 1) {
       leftArrow = "button-none";
-    } else if (this.state.idx === 4) {
+    } else if (this.state.idx === 8) {
       rightArrow = "button-none";
     }
     return (
@@ -69,18 +74,32 @@ class SplashComponent extends React.Component {
             </div>
 
             <div ref={this.photo3} className="card_part card_part-3">
-              <p className="slide-text">
+              <p className="slide-text flex-start">
                 When logging in, each player is entered in into the global
                 lobby. To challenge a player simply click on their name.
               </p>
             </div>
 
-            <div ref={this.photo4} className="card_part card_part-4">
+            <div ref={this.photo4} className="card_part card_part-4"></div>
+
+            <div ref={this.photo5} className="card_part card_part-5"></div>
+
+            <div ref={this.photo6} className="card_part card_part-6">
               <p className="slide-text">
                 Once in a game, the race is on! Problem solving is done in the
                 code editor.
               </p>
-              <p className="slide-text">Smack Talking is done in the chat!</p>
+            </div>
+
+            <div ref={this.photo7} className="card_part card_part-7">
+              <p className="slide-text">Smack talk is done in the chat!</p>
+            </div>
+
+            <div ref={this.photo8} className="card_part card_part-8">
+              <p className="slide-text">
+                When the game is over, both players are returned to the lobby
+              </p>
+              <p className="slide-text">Log in or sign up and have some fun!</p>
             </div>
           </div>
           <button
@@ -91,8 +110,12 @@ class SplashComponent extends React.Component {
           </button>
         </div>
         <div className="splash-controls-bottom">
-          <button className="splash-button">Login</button>
-          <button className="splash-button">Sign Up</button>
+          <Link to={"/login"} className="splash-button">
+            Login
+          </Link>
+          <Link to={"/signup"} className="splash-button">
+            Sign Up
+          </Link>
         </div>
       </div>
     );
